aeecc7d0aace6687007a1b1c3008eb0fa8061d5b
Kym Andrew <flamingpearl27@gmail.com>
2020-06-12 17:05:45 +0100

    page width restructured

--- footer.php
+++ footer.php
@@ -11,24 +11,31 @@
 
 ?>
 
-	</div><!-- #content -->
+			</div><!-- #content -->
+		</div>
+	</div>	
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'geekpack' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'geekpack' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'geekpack' ), 'geekpack', '<a href="https://www.geekpack.co">WP Rockstar</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+	<div class="footer-section full">
+		<div class="wrap">	
+
+			<footer id="colophon" class="site-footer inner">
+				<div class="site-info">
+					<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'geekpack' ) ); ?>">
+						<?php
+						/* translators: %s: CMS name, i.e. WordPress. */
+						printf( esc_html__( 'Proudly powered by %s', 'geekpack' ), 'WordPress' );
+						?>
+					</a>
+					<span class="sep"> | </span>
+						<?php
+						/* translators: 1: Theme name, 2: Theme author. */
+						printf( esc_html__( 'Theme: %1$s by %2$s.', 'geekpack' ), 'geekpack', '<a href="https://www.geekpack.co">WP Rockstar</a>' );
+						?>
+				</div><!-- .site-info -->
+			</footer><!-- #colophon -->
+		</div>
+	</div>
+
 
 <?php wp_footer(); ?>
 
